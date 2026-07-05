662ea306d464dd72ad118cff7e2613088313d98b
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2026-07-05 18:16:48 +0300

    added initial nodeagent

---
+++ src/exe/nodeagent/nodeagent.cc
@@ -0,0 +1,22 @@
+#include "core/common/signal_monitor.hh"
+#include "core/event/dispatcher_impl.hh"
+#include "core/io/tcp_listener.hh"
+#include "core/logging/log.hh"
+
+auto main() -> int {
+  carrot::event::DispatcherSharedPtr dispatcher = std::make_shared<carrot::event::DispatcherImpl>();
+  // Signal monitor must be activated before the logger thread, otherwise we might miss the signal.
+  carrot::common::SignalMonitor signal_monitor(dispatcher);
+
+  auto& logger = carrot::logging::Logger::GetInstance();
+  logger.Run();
+
+  LOG_REGISTER_THREAD();
+
+  carrot::io::TcpListener listener{dispatcher, 9090};
+
+  dispatcher->Run();
+  logger.Stop();
+
+  return 0;
+}
